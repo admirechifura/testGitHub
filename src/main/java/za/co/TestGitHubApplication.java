3145d74f9feeddae4787e01c4e1f83e085950322
Chifura, Admire : Barclays Africa Group Limited [X] <admire.chifura@absa.co.za>
2018-04-04 21:52:26 +0200

    Added a basic rest controller to the project which will display path variable

---
+++ src/main/java/za/co/TestGitHubApplication.java
@@ -0,0 +1,5 @@
+package za.co;
+
+@SpringBootApplication
+public class TestGitHubApplication {
+}
---
+++ src/main/java/za/co/web/rest/UserQueryRestController.java
@@ -0,0 +1,7 @@
+package za.co.web.rest;
+
+/**
+ * Created by admirechifura on 04/04/2018.
+ */
+public class UserQueryRestController {
+}
