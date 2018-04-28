061274fb8b0f57a095e45ddc4224a114655782d6
Chifura, Admire : Barclays Africa Group Limited [X] <admire.chifura@absa.co.za>
2018-04-28 22:44:39 +0200

    Added logic to make a rest call from the browser on port 8080

--- src/main/java/za/co/TestGitHubApplication.java
+++ src/main/java/za/co/TestGitHubApplication.java
@@ -1,5 +1,12 @@
 package za.co;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
 @SpringBootApplication
 public class TestGitHubApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(TestGitHubApplication.class, args);
+    }
 }
--- src/main/java/za/co/web/rest/UserQueryRestController.java
+++ src/main/java/za/co/web/rest/UserQueryRestController.java
@@ -1,7 +1,17 @@
 package za.co.web.rest;
 
-/**
- * Created by admirechifura on 04/04/2018.
- */
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/testGitHub/user")
 public class UserQueryRestController {
+
+    @RequestMapping(value = "/input/{id}", method = GET)
+    public String triggerCron(@PathVariable("id") final String id) {
+        return "You supplied value of:"+ id;
+    }
 }
